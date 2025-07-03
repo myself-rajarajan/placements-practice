2a2d85484c8a968aed720fd7ded3d291d31003e6
myself-rajarajan <rajarajannivetha@gmail.com>
2025-07-03 09:08:56 +0530

    Program to sort the numbers in the input string value in asc order

--- src/App.java
+++ src/App.java
@@ -1,21 +1,24 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String[] strarr = input.split("[:]");
+        // Now let us convert the String Array to a int Array
+        int[] numbers = new int[strarr.length];
+        int arrayPosition = 0;
+        for (String str : strarr){
+            numbers[arrayPosition++] = Integer.parseInt(str);
+        }
+        // Now sort the int Array
+        Arrays.sort(numbers);
 
-        //input to get as a string
-        System.out.println("What is your name?");
-        String name = scanner.nextLine();
-        
-
-        //input to get as an integer
-        System.out.println("How old are you?");
-        int age = scanner.nextInt();
-        
-        //output 
-        System.out.println("Hello " + name);
-        System.out.println("you are " + age + " years old.");
+        // Print the sorted numbers
+        for (int num : numbers){
+            System.out.println(num);
+        }
     }
 }
