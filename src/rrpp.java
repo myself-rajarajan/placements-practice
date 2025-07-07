c68f33263203cf86e6ed48ab00c7f5b1c956bddb
Rajarajan TM <108823654+myself-rajarajan@users.noreply.github.com>
2025-07-07 19:17:23 +0530

    Update rrpp.java

--- src/rrpp.java
+++ src/rrpp.java
@@ -8,7 +8,7 @@ public class rrpp {
         int n = sc.nextInt();
 
         for(int i = 0; i <= n; i++){
-            for(int j = 0; j < n - i + 1; j++){
+            for(int j = 0; j <= n - i + 1; j++){
                 System.out.print("*");
             }
             System.out.println();
